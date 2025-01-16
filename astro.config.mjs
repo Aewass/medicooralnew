2f85c450ed456a5ac71a796795853cacd894a258
Mirza@Evoila <mmuric@evoila.com>
2025-01-16 13:01:25 +0100

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://medicooral-as.com",
+  output: "server",
+  adapter: netlify({
+    edgeMiddleware: true,
+  }),
   integrations: [tailwind()],
 });
