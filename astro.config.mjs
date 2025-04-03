a314d91ef580128e288f14568934272c04b7f08f
Mirza@Evoila <mmuric@evoila.com>
2025-04-03 22:13:34 +0200

    revert to server

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import netlify from "@astrojs/netlify/functions";
 // https://astro.build/config
 export default defineConfig({
   site: "https://medicooral-as.com",
-  output: "static",
+  output: "server",
   adapter: netlify({
     edgeMiddleware: true,
   }),
