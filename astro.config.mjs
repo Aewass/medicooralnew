37d074a7703fea9a344d2faa10a5a5c6f47dbea5
Mirza@Evoila <mmuric@evoila.com>
2025-04-07 14:36:00 +0200

    try

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify/functions";
+import netlify from "@astrojs/netlify/static";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://medicooral-as.com",
   output: "static",
-  adapter: netlify({
-    edgeMiddleware: true,
-  }),
+  adapter: netlify(),
   integrations: [tailwind()],
 });
